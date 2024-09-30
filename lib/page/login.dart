b021d8e8d76de8ddbdde1ecb6ba03821b50e4189
Khanomtom <kunakron007@gmail.com>
2024-10-01 00:32:31 +0700

    zzz

--- lib/page/login.dart
+++ lib/page/login.dart
@@ -1,4 +1,6 @@
+import 'package:fast_feast/page/register.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -11,11 +13,94 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-          Text("data")
-        ],
+      backgroundColor: Colors.white,
+   
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            
+            Padding(
+              padding: const EdgeInsets.only(top: 100,bottom: 30),
+              child: 
+              Image.asset('asset/image/login.png'),
+            ),
+             SizedBox(
+              width: 365,
+               child: TextFormField(
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.phone, color: Colors.grey),
+                  labelText: 'Phone Number',
+                  labelStyle: const TextStyle(color: Colors.grey),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                    borderSide: BorderSide(color: Colors.grey.shade300),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                    borderSide: BorderSide(color: Colors.grey.shade300),
+                  ),
+                ),
+                           ),
+             ),
+            const SizedBox(height: 20.0),
+            // Password Field
+            SizedBox(
+              width: 365,
+              child: TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.lock, color: Colors.grey),
+                  labelText: 'Password',
+                  labelStyle: const TextStyle(color: Colors.grey),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                    borderSide: BorderSide(color: Colors.grey.shade300),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                    borderSide: BorderSide(color: Colors.grey.shade300),
+                  ),
+                ),
+              ),
+            ),
+                        const SizedBox(height: 25.0),
+
+            ElevatedButton(
+              onPressed: () {
+                // Handle sign in action
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.lightBlue, // Background color
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0), // Rounded corners
+                ),
+                padding: const EdgeInsets.symmetric(vertical: 18.0, horizontal: 150.0), // Button size
+              ),
+              child: const Text(
+                'SIGN IN',
+                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            // Create Account Button
+            OutlinedButton(
+              onPressed: () {
+                Get.to( RegisterPage());
+              },
+              style: OutlinedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0), // Rounded corners
+                ),
+                padding: const EdgeInsets.symmetric(vertical: 18.0, horizontal: 125.0), // Button size
+                side: BorderSide(color: Colors.grey.shade300), // Border color
+              ),
+              child: const Text(
+                'Create Account',
+                style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              ),
+            ),
+            ],
+        ),
       ),
     );
   }
---
+++ lib/page/register.dart
@@ -0,0 +1,150 @@
+import 'dart:developer';
+
+import 'package:fast_feast/widget/RegisRider.dart';
+import 'package:fast_feast/widget/RegisUser.dart';
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  List<bool> isSelected = [true, false]; // For User and Rider toggle buttons
+  Widget widgetField = const RegisUser();
+  @override
+  bool isUserSelected = true; // By default, 'User' is selected
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 35),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Column(
+                crossAxisAlignment:
+                    CrossAxisAlignment.start, // Aligns content to the left
+                children: [
+                  const Text(
+                    'Let\'s Create Your Account',
+                    style:
+                        TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 8.0),
+                  const Text('Select Account Type'),
+                  Row(
+      children: [
+        isUserSelected
+            ? ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    isUserSelected = true; // Mark 'User' as selected
+                    widgetField = const RegisUser();
+                  });
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.lightBlue, // Background color
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0), // Rounded corners
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 30.0), // Button size
+                ),
+                child: const Text(
+                  'User',
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              )
+            : OutlinedButton(
+                onPressed: () {
+                  setState(() {
+                    isUserSelected = true; // Mark 'User' as selected
+                    widgetField = const RegisUser();
+                  });
+                },
+                style: OutlinedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0), // Rounded corners
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 30.0), // Button size
+                  side: BorderSide(color: Colors.grey.shade300), // Border color
+                ),
+                child: const Text(
+                  'User',
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold),
+                ),
+              ),
+        const SizedBox(width: 15),
+        !isUserSelected
+            ? ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    isUserSelected = false; // Mark 'Rider' as selected
+                    widgetField = const RegisRider();
+                  });
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.lightBlue, // Background color
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0), // Rounded corners
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 30.0), // Button size
+                ),
+                child: const Text(
+                  'Rider',
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              )
+            : OutlinedButton(
+                onPressed: () {
+                  setState(() {
+                    isUserSelected = false; // Mark 'Rider' as selected
+                    widgetField = const RegisRider();
+                  });
+                },
+                style: OutlinedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0), // Rounded corners
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 30.0), // Button size
+                  side: BorderSide(color: Colors.grey.shade300), // Border color
+                ),
+                child: const Text(
+                  'Rider',
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold),
+                ),
+              ),
+      ],
+    )
+                ],
+              ),
+
+              const SizedBox(height: 16.0),
+              // Profile Picture Placeholder
+              CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.grey.shade300,
+                child: const Icon(Icons.person, size: 60, color: Colors.grey),
+              ),
+
+              const SizedBox(height: 16.0),
+              // Input Fields
+              widgetField,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
